961c0cec6e3b2401f3d696f835aa773400829901
Halim Qarroum <hqarroum@awox.com>
2015-08-11 17:12:38 +0200

    Removing the obj artifacts directory

--- mdns/src/main/obj/local/armeabi/objs/jdns_sd/mDNSShared/GenLinkedList.o.d
+++
@@ -1,5 +0,0 @@
-/home/halim/repositories/android-mdns/mdns/src/main/obj/local/armeabi/objs/jdns_sd/mDNSShared/GenLinkedList.o: \
- /home/halim/repositories/android-mdns/mdns/src/main/jni/mDNSShared/GenLinkedList.c \
- /home/halim/repositories/android-mdns/mdns/src/main/jni/mDNSShared/GenLinkedList.h
-
-/home/halim/repositories/android-mdns/mdns/src/main/jni/mDNSShared/GenLinkedList.h:
